a505d733e9587370a7345162fb88ec4847f7ec1a
Vincezo <Vvinciguerra6@gmail.com>
2026-08-06 23:30:15 +0200

    Commit to test ssh reachibility

---
+++ PE_ROUTERS/Scripts/run.py
@@ -0,0 +1,24 @@
+import ConnectHandler
+import os
+import pathlib
+
+
+CREDENTIALS="../../CREDENTIALS/mpl3_vpn_credentials.txt"
+
+def __main__():
+
+    handlers = []
+
+    with open(CREDENTIALS) as f:
+        lines = f.readlines()
+
+        for line in lines:
+            device_type = line.split(',')[0].strip()
+            ip = line.split(',')[1].strip()
+            username = line.split(',')[2].strip()
+            password = line.split(',')[3].strip()
+
+        handlers.append(ConnectHandler.connect_to_device(device_type,ip,username,password))
+
+
+
